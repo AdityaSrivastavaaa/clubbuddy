3db6ba4ae98cc88e179b38ccb9c7e5b4bacbc56a
Aditya Srivastava <srivastavaaditya015@gmail.com>
2025-02-28 04:22:22 +0530

    some changes

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { ReactTyped } from "react-typed"; // ✅ Correct import
-import backgroundImage from "../../public/assets/ClubBackground.webp";
-import underline from "../../public/assets/underline.svg";
-import appstore from "../../public/assets/AppStore.svg";
-import playstore from "../../public/assets/PlayStore.svg";
+// import backgroundImage from "../../public/assets/ClubBackground.webp";
+// import underline from "../../public/assets/underline.svg";
+// import appstore from "../../public/assets/AppStore.svg";
+// import playstore from "../../public/assets/PlayStore.svg";
 
 function MainBanner() {
   const [showTyping] = useState(true);
@@ -11,7 +11,7 @@ function MainBanner() {
   return (
     <div
       className="relative h-screen flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
+      style={{ backgroundImage: `url(/assets/ClubBackground.webp)` }}
     >
       {/* Whitish Blur Overlay for Better Visibility */}
       <div className="absolute inset-0 backdrop-blur-xs bg-white/20"></div>
@@ -33,17 +33,17 @@ function MainBanner() {
         <p className="text-lg md:text-xl font-medium text-red-50 max-w-2xl mx-auto ">
           Dating designed for real connections.
         </p>
-        <img src={underline} className="mx-auto h-3 my-2" alt="Underline" />
+        <img src="/assets/underline.svg" className="mx-auto h-3 my-2" alt="Underline" />
 
         {/* Store Buttons */}
         <div className="flex justify-center mt-6 gap-4">
           <img
-            src={appstore}
+            src="/assets/AppStore.svg"
             className="h-12 md:h-14 drop-shadow-lg cursor-pointer hover:scale-105 transition"
             alt="App Store"
           />
           <img
-            src={playstore}
+            src="/assets/PlayStore.svg"
             className="h-12 md:h-14 drop-shadow-lg cursor-pointer hover:scale-105 transition"
             alt="Play Store"
           />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import brandlogo from "../../public/assets/clubbuddy.png"; // Ensure correct path
+// import brandlogo from "../../public/assets/clubbuddy.png"; // Ensure correct path
 
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-zinc-50 shadow-md py-4 px-6 flex items-center z-10 h-16">
       <div className="flex items-center">
-        <img src={brandlogo} alt="Brand Logo" className="w-24 h-auto object-contain mr-2" />
+        <img src="/assets/clubbuddy.png" alt="Brand Logo" className="w-24 h-auto object-contain mr-2" />
       </div>
     </nav>
   );
